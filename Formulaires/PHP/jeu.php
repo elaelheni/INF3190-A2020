82c3ed5cdb6ebdef0bbe2f1a0f31169e6b176b49
elaelheni <elheni.ela@gmail.com>
2020-10-05 23:27:06 +0100

    correction ex1

--- Formulaires/PHP/jeu.php
+++ Formulaires/PHP/jeu.php
@@ -1,3 +1,25 @@
+<?php
+$aDeviner = 300;
+$echec = null;
+$succes = null;
+$chiffre = null;
+
+if (isset($_GET["chiffre"])){
+    $chiffre = (int)$_GET["chiffre"];
+    if ($chiffre < $aDeviner){
+        $echec = "Oups! Trop petit";
+    }
+    elseif ($chiffre > $aDeviner){
+        $echec = "Oups! Trop grand";
+    }
+    else {
+        $succes = "Bravo! Vous avez trouvé le chiffre <strong>$aDeviner</strong>";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,5 +30,21 @@
 <body>
     <h3>Essayez de deviner un nombre entre 0 et 1000</h4>
     <br>
+    <?php if($echec):?>
+    <div>
+        <?= $echec ?>
+    </div>
+    <?php elseif($succes):?>
+    <div>
+        <?= $succes ?>
+    </div>
+    <? endif ?>
+
+
+    <form action="jeu.php" method="GET">
+    <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?= $chiffre?>">
+    <button type="submit">Deviner</button>
+    </form>
+
 </body>
 </html>
