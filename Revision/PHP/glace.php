fd785040c49199a6f0f1c435476a03df02553a95
elaelheni <elheni.ela@gmail.com>
2020-10-30 01:03:34 +0100

    ajout des enregistrements

--- Revision/PHP/glace.php
+++ Revision/PHP/glace.php
@@ -30,48 +30,44 @@ $supplements = [
 $total = 0;
 
 //Pour valider que mes variables existent
-if($_SERVER["REQUEST_METHOD"] ==  "POST"){
-if (!isset($_POST['parfum']) || !isset($_POST['supplement']) || !isset($_POST['cornet'])){
-    $message = "Les champs sont obligatoires";
-    http_response_code(400);
-    exit;
-
-}
+if ($_SERVER["REQUEST_METHOD"] ==  "POST") {
+    if (!isset($_POST['parfum']) || !isset($_POST['supplement']) || !isset($_POST['cornet'])) {
+        $message = "Les champs sont obligatoires";
+        http_response_code(400);
+        exit;
+    }
 }
 
 //Pour valider que mes variables ne sont pas vides
-if(empty($_POST['parfum']) || empty($_POST['supplement']) || empty($_POST['cornet'])){
+if (empty($_POST['parfum']) || empty($_POST['supplement']) || empty($_POST['cornet'])) {
     $message = "Valeur est manquante";
-    
 }
 
 
 //Pour chaque parfum coché par mon client, je récupère son prix à partir de ma liste de parfums déclarée ci haut et je l'ajoute a mon total
-if (isset($_POST['parfum'])){
-    foreach($_POST['parfum'] as $parfum){
-        if(isset($parfums[$parfum])){
-            $total+=$parfums[$parfum];
+if (isset($_POST['parfum'])) {
+    foreach ($_POST['parfum'] as $parfum) {
+        if (isset($parfums[$parfum])) {
+            $total += $parfums[$parfum];
         }
-
     }
 }
 
 //Pour chaque supplement coché par mon client, je récupère son prix à partir de ma liste de supplements déclarée ci haut et je l'ajoute a mon total
-if (isset($_POST['supplement'])){
-    foreach($_POST['supplement'] as $supplement){
+if (isset($_POST['supplement'])) {
+    foreach ($_POST['supplement'] as $supplement) {
         //Pour valider que le supplement existe dans ma liste => Pour ne pas permettre à l'utilisateur d'entrer n'importe quoi
-        if(isset($supplements[$supplement])){
-            $total+=$supplements[$supplement];
+        if (isset($supplements[$supplement])) {
+            $total += $supplements[$supplement];
         }
-
     }
 }
 
 //Pour le type de cornet selectionné, cherche son prix dans ma liste de cornets déclarée ci-haut et ajoute le à mon total
-if (isset($_POST['cornet'])){
+if (isset($_POST['cornet'])) {
     $cornet = $_POST['cornet'];
-    if(isset($cornet)){
-        $total+=$cornets[$cornet];
+    if (isset($cornet)) {
+        $total += $cornets[$cornet];
     }
 }
 //stocker mon total dans un cookie pour pouvoir l'utiliser plus tard
@@ -85,16 +81,18 @@ header("Location: prix.php");
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Revision pour l'examen">
     <title>Glace</title>
 </head>
+
 <body>
     <!-- debogger pour afficher les choix de mon client -->
-    <?php var_dump($_POST)?>
+    <?php var_dump($_POST) ?>
     <!-- afficher mon message d'erreur si il existe -->
-    <?php if($message) :?>
+    <?php if ($message) : ?>
         <p>
             <?= $message ?>
         </p>
@@ -104,47 +102,47 @@ header("Location: prix.php");
 
     <form action="/glace.php" method="POST">
 
-    <h4>Choisissez votre parfum :</h4>
-    <br>
-    <?php foreach($parfums as $parfum => $prix) :?>
-        <div class="checkbox">
-            <label for="champ-parfum">
-                <?= checkbox('parfum', $parfum, $_POST) ?>
+        <h4>Choisissez votre parfum :</h4>
+        <br>
+        <?php foreach ($parfums as $parfum => $prix) : ?>
+            <div class="checkbox">
+                <label for="champ-parfum">
+                    <?= checkbox('parfum', $parfum, $_POST) ?>
 
-                <?=  $parfum ?> - <?= $prix ?>
+                    <?= $parfum ?> - <?= $prix ?>
 
-            </label>
-        </div>
+                </label>
+            </div>
 
-    <?php endforeach; ?>
-    
-    <h4>Choisissez votre cornet:</h4>
-    <br>
-    <?php foreach($cornets as $cornet => $prix) :?>
-        <div class="checkbox">
-            <label for="champ-parfum">
-                <?= radio('cornet', $cornet, $_POST) ?>
+        <?php endforeach; ?>
 
-                <?=  $cornet ?> - <?= $prix ?>
+        <h4>Choisissez votre cornet:</h4>
+        <br>
+        <?php foreach ($cornets as $cornet => $prix) : ?>
+            <div class="checkbox">
+                <label for="champ-parfum">
+                    <?= radio('cornet', $cornet, $_POST) ?>
 
-            </label>
-        </div>
+                    <?= $cornet ?> - <?= $prix ?>
 
-    <?php endforeach; ?>
-    
-    <h4>Choisissez votre supplement :</h4>
-    <br>
-    <?php foreach($supplements as $supplement => $prix) :?>
-        <div class="checkbox">
-            <label for="champ-parfum">
-                <?= checkbox('supplement', $supplement, $_POST) ?>
+                </label>
+            </div>
 
-                <?=  $supplement ?> - <?= $prix ?>
+        <?php endforeach; ?>
 
-            </label>
-        </div>
+        <h4>Choisissez votre supplement :</h4>
+        <br>
+        <?php foreach ($supplements as $supplement => $prix) : ?>
+            <div class="checkbox">
+                <label for="champ-parfum">
+                    <?= checkbox('supplement', $supplement, $_POST) ?>
 
-    <?php endforeach; ?>
+                    <?= $supplement ?> - <?= $prix ?>
+
+                </label>
+            </div>
+
+        <?php endforeach; ?>
         <br>
         <button type="submit">Composez votre glace</button>
 
